831573e353f0cf4f9fa63cea759ea0e15ff6e54c
Googa <guganik208@gmail.com>
2026-08-19 18:46:10 +0500

    Auto backup: 2026-08-19 18:46:10

--- lib/screens/load_screen.dart
+++ lib/screens/load_screen.dart
@@ -4,6 +4,8 @@ import 'package:mess_prototype/screens/auth_screen.dart';
 import 'package:mess_prototype/services/app_initializer.dart';
 
 import 'package:mess_prototype/screens/main_screen.dart';
+import 'package:mess_prototype/services/connection_checker.dart';
+import 'package:provider/provider.dart';
 
 class LoadScreen extends StatefulWidget {
   const LoadScreen({super.key});
@@ -15,8 +17,6 @@ class LoadScreen extends StatefulWidget {
 class LoadScreenState extends State<LoadScreen> {
   final initializer = AppInitializer();
 
-  bool isInit = false;
-
   @override
   void initState() {
     super.initState();
@@ -24,40 +24,36 @@ class LoadScreenState extends State<LoadScreen> {
   }
 
   Future<void> startApp() async {
-    while(!isInit) {
-      try {
-        final user = await initializer.hasUser();
-        final settings = await initializer.loadSettings();
+    final checker = context.watch<ConnectionChecker>();
+    try {
+      final user = await initializer.hasUser();
+      final settings = await initializer.loadSettings();
 
-        if (!mounted) return;
-        
-        initializer.dispose();
-        isInit = true;
+      if (!mounted) return;
+      
+      initializer.dispose();
 
-        if (user != null) {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (_) => MainScreen(
-                user: user,
-                settings: settings,
-              )
+      if (user != null) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => MainScreen(
+              user: user,
+              settings: settings,
             )
-          );
-        } else {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (_) => AuthScreen()
-            )
-          );
-        }
-      } catch(e) {
-        print('Сервер недоступен');
-        print(e);
-
-        await Future.delayed(Duration(seconds: 1));
+          )
+        );
+      } else {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (_) => AuthScreen()
+          )
+        );
       }
+    } catch(e) {
+      print('Сервер недоступен');
+      print(e);
     }
   }
 
