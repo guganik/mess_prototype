5814c9e571d0b16099e7261cdf00a2ca2450192a
Googa <guganik208@gmail.com>
2026-08-21 11:01:11 +0500

    Auto backup: 2026-08-21 11:01:11

--- lib/models/account_sync.dart
+++ lib/models/account_sync.dart
@@ -1,23 +1,43 @@
+import 'package:mess_prototype/models/chat.dart';
 import 'package:mess_prototype/models/user.dart';
 
 class AccountSync {
   final User user;
+  final List<Chat> chats;
 
   const AccountSync({
     required this.user,
+    required this.chats,
   });
 
-  factory AccountSync.fromJson(Map<String, dynamic> json) {
+  factory AccountSync.fromJson(
+    Map<String, dynamic> json,
+  ) {
     final userData = json['user'];
 
     if (userData is! Map<String, dynamic>) {
       throw const FormatException(
-        'Invalid account sync response',
+        'Invalid account sync user',
+      );
+    }
+
+    final chatsData = json['chats'];
+
+    if (chatsData is! List) {
+      throw const FormatException(
+        'Invalid account sync chats',
       );
     }
 
     return AccountSync(
       user: User.fromJson(userData),
+      chats: chatsData
+          .map(
+            (item) => Chat.fromJson(
+              item as Map<String, dynamic>,
+            ),
+          )
+          .toList(),
     );
   }
 }
