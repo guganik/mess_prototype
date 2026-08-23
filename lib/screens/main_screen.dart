8820941b003feff941ce575b9f4f51141cd89b01
Googa <guganik208@gmail.com>
2026-08-23 11:11:10 +0500

    Auto backup: 2026-08-23 11:11:10

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -594,7 +594,7 @@ class LeftPanelState extends State<LeftPanel> {
               ),
               SizedBox(height: 4,),
               Text(
-                'Version 1.0.4.0',
+                'Version 1.0.4.1',
                 style: TextStyle(
                   fontSize: 10,
                   color: Colors.grey[500]
