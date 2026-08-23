e14394bbb53c71117e2a86da95a063ef9f1f8700
Googa <guganik208@gmail.com>
2026-08-23 22:06:11 +0500

    Auto backup: 2026-08-23 22:06:11

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -487,7 +487,7 @@ class ApiService {
   Future<List<ChatMessage>> getMessages({
     required String token,
     required int chatId,
-    int limit = 50,
+    int limit = 2000,
     int? beforeId,
   }) async {
     final uri = Uri.parse(
