2f639a2d63d55f543f7dd91ebedb86e7a4c70812
Googa <guganik208@gmail.com>
2026-08-23 20:35:26 +0500

    Auto backup: 2026-08-23 20:35:26

--- test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command ^
-"$p = Start-Process powershell.exe -Verb RunAs -PassThru -Wait -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%~dp0test.ps1""'; exit $p.ExitCode"
-
-pause
